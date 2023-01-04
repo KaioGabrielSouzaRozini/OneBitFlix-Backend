e69bcd3589f1ee2c6e72d0bcf24077309f38e27b
bituka <bitukacomk@gmail.com>
2023-01-04 12:15:36 -0300

    personalizando o Dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -5,7 +5,7 @@ import AdminJsExpress from "@adminjs/express";
 import AdminJsSequelize from "@adminjs/sequelize";
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
-import { User } from "../models";
+import { Category, Episode, User, Course } from "../models";
 import bcrypt from "bcrypt";
 import { locale } from "./locale";
 
@@ -37,6 +37,22 @@ export const adminJs = new AdminJs({
     },
   },
   locale: locale,
+  dashboard: {
+    component: AdminJs.bundle("./components/Dashboard"),
+    handler: async (req, res, context) => {
+      const courses = await Course.count();
+      const episodes = await Episode.count();
+      const categories = await Category.count();
+      const standardUsers = await User.count({ where: { role: "user" } });
+
+      res.json({
+        Cursos: courses,
+        Episódios: episodes,
+        Categorias: categories,
+        Usuários: standardUsers,
+      });
+    },
+  },
 });
 
 export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(

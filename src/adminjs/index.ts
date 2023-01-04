e789373ebeb6b162b9a035375380040abd365589
bituka <bitukacomk@gmail.com>
2023-01-04 11:41:29 -0300

    Adicionando traduções

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -7,6 +7,7 @@ import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
 import { User } from "../models";
 import bcrypt from "bcrypt";
+import { locale } from "./locale";
 
 AdminJs.registerAdapter(AdminJsSequelize);
 
@@ -35,6 +36,7 @@ export const adminJs = new AdminJs({
       },
     },
   },
+  locale: locale,
 });
 
 export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(

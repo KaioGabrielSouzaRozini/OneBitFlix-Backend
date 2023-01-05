9f3370391e1a368e2eddb04b810ba4953ffc5c3b
bituka <bitukacomk@gmail.com>
2023-01-05 12:59:23 -0300

    Obtendo os lançamentos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,8 +2,6 @@ import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
 export const coursesController = {
   // GET /courses/features
-
-  // GET /courses/:id
   featured: async (req: Request, res: Response) => {
     try {
       const featuredCourses = await courseService.getRandomFeaturedCourses();
@@ -14,6 +12,20 @@ export const coursesController = {
       }
     }
   },
+
+  // GET /courses/newest
+  newest: async (req: Request, res: Response) => {
+    try {
+      const newestCourses = await courseService.getTopTenNewest();
+      return res.json(newestCourses);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
+  // GET /courses/:id
   show: async (req: Request, res: Response) => {
     const { id } = req.params;
 
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -35,4 +35,13 @@ export const courseService = {
 
     return randomFeaturedCourses.slice(0, 3);
   },
+
+  getTopTenNewest: async () => {
+    const courses = await Course.findAll({
+      limit: 10,
+      order: [["created_at", "DESC"]],
+    });
+
+    return courses;
+  },
 };

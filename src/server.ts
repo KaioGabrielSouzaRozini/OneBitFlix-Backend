fe02619bdd603093cd8af80818aaa3f59400980d
bituka <bitukacomk@gmail.com>
2023-01-05 11:03:32 -0300

    Obtendo cursos de uma categoria

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { sequelize } from "./database";
 import { adminJs, adminJsRouter } from "./adminjs";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.use(adminJs.options.rootPath, adminJsRouter);
+
+app.use(router);
 
 const PORT = process.env.port || 3000;
 

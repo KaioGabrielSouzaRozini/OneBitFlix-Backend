c3420fefeab3b1221f4202cd016a6d9578118f45
bituka <bitukacomk@gmail.com>
2023-01-06 12:23:44 -0300

    Registro de usuários

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.use(express.json());
+
 app.use(adminJs.options.rootPath, adminJsRouter);
 
 app.use(router);
@@ -14,6 +16,7 @@ app.use(router);
 const PORT = process.env.port || 3000;
 
 app.listen(PORT, async () => {
+  ("");
   await sequelize.authenticate().then(() => {
     console.log("DB connection successfull.");
   });

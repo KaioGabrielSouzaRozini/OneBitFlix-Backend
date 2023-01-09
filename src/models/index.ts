f713e8ce38d120f91db361ca99e7d9e6c7305764
bituka <bitukacomk@gmail.com>
2023-01-09 09:35:00 -0300

    Adicionando favoritos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,14 +1,22 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
+import { Favorite } from "./Favorite";
 import { User } from "./User";
 
 Category.hasMany(Course, { as: "courses" });
 
 Course.belongsTo(Category);
-
+Course.belongsToMany(User, { through: Favorite });
 Course.hasMany(Episode, { as: "episodes" });
+Course.hasMany(Favorite, { as: "FavoritesUsers", foreignKey: "course_id" });
 
 Episode.belongsTo(Course);
 
-export { Category, Course, Episode, User };
+Favorite.belongsTo(Course);
+Favorite.belongsTo(User);
+
+User.belongsToMany(Course, { through: Favorite });
+User.hasMany(Favorite, { as: "FavoritesCourses", foreignKey: "user_id" });
+
+export { Category, Course, Episode, Favorite, User };

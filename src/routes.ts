40308d714014a999b6662de264db230d6f6a9f82
bituka <bitukacomk@gmail.com>
2023-01-09 11:04:58 -0300

    Criando o gostei

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { categoriesController } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
 import { favoriteController } from "./controllers/favoritesController";
+import { likesController } from "./controllers/likesController";
 import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth";
 
 const router = express.Router();
@@ -24,5 +25,7 @@ router.get("/episodes/stream", ensureAuthViaQuery, episodesController.stream);
 router.get("/favorites", ensureAuth, favoriteController.index);
 router.post("/favorites", ensureAuth, favoriteController.save);
 router.delete("/favorites/:id", ensureAuth, favoriteController.delete);
+
+router.post("/likes", ensureAuth, likesController.save);
 
 export { router };
---
+++ src/services/likeService.ts
@@ -0,0 +1,11 @@
+import { Like } from "../models";
+
+export const likeService = {
+  create: async (userId: number, courseId: number) => {
+    const like = await Like.create({
+      userId,
+      courseId,
+    });
+    return like;
+  },
+};

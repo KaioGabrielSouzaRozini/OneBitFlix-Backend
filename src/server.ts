ec915c987050d074abdee974bfdf0eadaf32d4ac
bituka <bitukacomk@gmail.com>
2023-01-12 13:36:45 -0300

    Adicionando o cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { sequelize } from "./database";
 import { adminJs, adminJsRouter } from "./adminjs";
 import { router } from "./routes";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.static("public"));
 

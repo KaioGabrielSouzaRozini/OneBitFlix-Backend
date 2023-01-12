fb9aa77a80f7bf6a41d5ed0a23937a7c14320f6f
bituka <bitukacomk@gmail.com>
2023-01-12 09:05:24 -0300

    Obtendo informações da conta de usuário

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -3,6 +3,18 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userService } from "../services/userService";
 
 export const usersController = {
+  /// GET /users/current
+  show: async (req: AuthenticatedRequest, res: Response) => {
+    const currentUser = req.user!;
+
+    try {
+      return res.json(currentUser);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
   // GET /users/current/watching
   watching: async (req: AuthenticatedRequest, res: Response) => {
     const { id } = req.user!;

faf7dbf82a6cec6239ae96097f40ae15194bd233
bituka <bitukacomk@gmail.com>
2023-01-12 09:57:39 -0300

    Atualizando a senha

--- src/routes.ts
+++ src/routes.ts
@@ -46,5 +46,10 @@ router.delete("/likes/:id", ensureAuth, likesController.delete);
 router.get("/users/current", ensureAuth, usersController.show);
 router.get("/users/current/watching", ensureAuth, usersController.watching);
 router.put("/users/current", ensureAuth, usersController.update);
+router.put(
+  "/users/current/password",
+  ensureAuth,
+  usersController.updatePassword
+);
 
 export { router };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -62,6 +62,21 @@ export const userService = {
     return updatedUsers[0];
   },
 
+  updatePassword: async (id: string | number, password: string) => {
+    const [affectedRows, updatedUsers] = await User.update(
+      {
+        password,
+      },
+      {
+        where: { id },
+        individualHooks: true,
+        returning: true,
+      }
+    );
+
+    return updatedUsers[0];
+  },
+
   getKeepWatchingList: async (id: number) => {
     const userWithWatchingEpisodes = await User.findByPk(id, {
       include: {
